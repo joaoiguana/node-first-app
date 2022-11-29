5d0b522de5fb80b8b8f6be9ecf79ad63dd8e3e9b
joao garcia <jiboiadamor@gmail.com>
2022-11-29 12:31:44 +0000

    working with the file system module

--- app.js
+++ app.js
@@ -33,5 +33,10 @@
 
 const fs = require('fs');
 
-const files = fs.readdirSync('./');
-console.log(files);
+// const files = fs.readdirSync('./');  ---Synchronous way
+// console.log(files);
+
+fs.readdir('./', function(err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files)
+});
